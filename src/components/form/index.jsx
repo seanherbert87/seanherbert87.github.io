4f9e4f3a193da68c6c60934995a2262882b2e2ff
seanherbert87 <4790716+seanherbert87@users.noreply.github.com>
2021-11-28 15:50:20 -0600

    Update index.jsx

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -7,7 +7,7 @@ const Form = ({}) => {
       <Section title="Contact Me">
         <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
             <p class="hidden">
-                <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                <label>Don't fill this out if you're human: <input name="bot-field" /></label>
             </p>
             <p>
                 <label>Name: <input type="text" name="name" /></label>
