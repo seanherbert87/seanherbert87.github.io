ff0a3bc462652e650b9905dc74071f7f0faa2c21
seanherbert87 <4790716+seanherbert87@users.noreply.github.com>
2021-11-28 14:24:06 -0600

    Init Contact Form

---
+++ src/components/form/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Section from '../section';
+
+const Form = ({}) => {
+    return (
+      <Section title="Contact Me">
+        <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true">
+            <p class="hidden">
+                <label>Don’t fill this out if you’re human: <input name="bot-field" /></label>
+            </p>
+            <p>
+                <label>Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email: <input type="text" name="email" /></label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
+      </Section>
+    );
+  };
+  
+  export default Form;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,35 @@
+import { graphql } from 'gatsby';
+import React from 'react';
+
+import Header from '../components/header';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const Index = ({ data }) => {
+  
+  return (
+    <Layout>
+      <SEO title="Contact Me" />
+      <Header metadata={data.site.siteMetadata} />
+      <Form />
+    </Layout>
+  );
+};
+
+export default Index;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        name
+        title
+        description
+        about
+        author
+        github
+        linkedin
+      }
+    }
+  }
+`;
