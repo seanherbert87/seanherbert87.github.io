e52f2517d9137cc0ea08d3718cef84f7f5c5227b
seanherbert87 <4790716+seanherbert87@users.noreply.github.com>
2021-11-28 17:18:18 -0600

    Update index.jsx

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -4,7 +4,8 @@ import Section from '../section';
 const Form = ({}) => {
     return (
       <Section title="Contact Me">
-        <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+        <form name="contact" netlify-honeypot="bot-field" netlify hidden>
+            <input type="hidden" name="form-name" value="contact" />
             <p class="hidden">
                 <label>Don't fill this out if you're human: <input name="bot-field" /></label>
             </p>
