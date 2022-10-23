f594a7d6cfce50b61eb48cf284bcfa2d7002e394
Sean Herbert <4790716+seanherbert87@users.noreply.github.com>
2022-10-23 18:07:08 -0500

    Blog cleanup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,6 @@ import React from 'react';
 import Header from '../components/header';
 import Layout from '../components/layout';
 import SectionAbout from '../components/section-about';
-import SectionBlog from '../components/section-blog';
 import SectionExperience from '../components/section-experience';
 import SectionProjects from '../components/section-projects';
 import SectionSkills from '../components/section-skills';
