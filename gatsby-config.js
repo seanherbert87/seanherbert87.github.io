afa05eae7a0476e3baa1c816b573bd627e526f25
Sean Herbert <4790716+seanherbert87@users.noreply.github.com>
2022-10-23 10:31:21 -0500

    More blog cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,13 +65,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
