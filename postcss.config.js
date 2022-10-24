52635bf7858a9167007d249a777513f0d1b759a2
seanherbert87 <4790716+seanherbert87@users.noreply.github.com>
2022-10-24 16:17:59 -0500

    misc updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    postcss: process.env.NODE_ENV === 'production' ? {} : false,
     tailwindcss: {},
     autoprefixer: {},
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],  
   future: {
-      removeDeprecatedGapUtilities: true,
-      purgeLayersByDefault: true,
-    },
-    purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
-    theme: {
-      extend: {
-        screens: {
-          xs: '480px',
-        },
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      screens: {
+        xs: '480px',
       },
     },
-    variants: {},
-    plugins: [],
-  };
+  },
+  variants: {},
+  plugins: [],
+};
   
