9b7e34e9c0873ae9ca5af1de3dba0359ea90344c
seanherbert87 <4790716+seanherbert87@users.noreply.github.com>
2022-10-25 09:13:27 -0500

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,14 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],  
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {
-      screens: {
-        xs: '480px',
-      },
-    },
+      extend: {},
   },
-  variants: {},
+  variants: {
+      extend: {},
+  },
   plugins: [],
 };
   
